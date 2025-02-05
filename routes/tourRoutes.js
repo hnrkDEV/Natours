c3c0ca564b022fef4423376d3a60eb6ec7de2122
João Henrique <jhenrique.caval@hotmail.com>
2025-02-05 01:42:21 -0300

    fix

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,9 +7,11 @@ router
 .route('/top-5-cheap')
 .get(tourController.aliasTopTours, tourController.getAllTours);
 
-router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/tour-stats')
+.get(tourController.getTourStats);
 
-/* router.param('id', tourController.checkID); */
+router.route('/monthly-plan/:year')
+.get(tourController.getMonthlyPlan);
 
 router
   .route('/')
