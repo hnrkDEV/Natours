f84e733d8b77c1b1a8d305ad4d58463399778b53
João Henrique <jhenrique.caval@hotmail.com>
2025-02-05 00:37:33 -0300

    initial commit

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel')
+const path = require("path");
+
+const filePath = path.join(__dirname, "tours-simple.json");
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true, 
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('DB Connection successful!')
+});
+
+// READING JSON FILE 
+const tours = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+
+// IMPORTING DATA INTO DB
+const importData = async () => {
+    try {
+        await Tour.create(tours)
+        console.log('Data successfully loaded!')
+    } catch (error) {
+        console.log(error)
+    };
+    process.exit();
+};
+
+// DELETE ALL DATA FROM DB
+const deleteData = async() => {
+    try {
+        await Tour.deleteMany();
+        console.log('Data successfully deleted!');
+    } catch (error) {
+        console.log(error)
+    };
+    process.exit();
+};
+
+if(process.argv[2] === '--import'){
+    importData();
+}   else if (process.argv[2] === '--delete') {
+    deleteData();
+};
