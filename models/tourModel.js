ee56aad59c408a9f4c73e7c144e81a002630246a
João Henrique <jhenrique.caval@hotmail.com>
2025-02-05 19:37:24 -0300

    Documents Middlewares add

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+
 const tourSchema =  new mongoose.Schema({
     name: {
       type: String,
@@ -6,6 +8,7 @@ const tourSchema =  new mongoose.Schema({
       unique: true,
       trim: true
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration']
@@ -50,8 +53,26 @@ const tourSchema =  new mongoose.Schema({
       default: Date.now()
     },
     startDates: [Date]
+  }, {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
   });
   
+  tourSchema.virtual('durationWeeks').get(function(){
+    return this.duration / 7;
+  });
+
+  // DOCUMENT MIDDLEWARE: runb before .save() and .create();
+  tourSchema.pre('save', function(next) {
+    this.slug = slugify(this.name, {lower: true});
+    next();
+  });
+  
+/*   tourSchema.post('save', function(doc, next) {
+    console.log(doc);
+    next();
+  }) */
+
   const Tour = mongoose.model('Tour', tourSchema);
 
   module.exports = Tour;
