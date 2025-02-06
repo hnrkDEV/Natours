b5efd5f459859e948f069eac800bf13cfbba77b3
João Henrique <jhenrique.caval@hotmail.com>
2025-02-06 02:49:01 -0300

    Validators added

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,7 +6,9 @@ const tourSchema =  new mongoose.Schema({
       type: String,
       required: [true, 'A tour must have a name'],
       unique: true,
-      trim: true
+      trim: true,
+      maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+      minlength: [10, 'A tour name must have more or equal than 10 characters']
     },
     slug: String,
     duration: {
@@ -19,11 +21,17 @@ const tourSchema =  new mongoose.Schema({
     },
     difficulty: {
       type: String,
-      required: [true, 'A tour must have a difficulty']
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium or difficult.'
+      },
     },
     ratingsAverage: {
       type: Number,
-      default: 4.5
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0']
     },
     ratingsQuantity:  {
       type: Number,
@@ -33,7 +41,15 @@ const tourSchema =  new mongoose.Schema({
       type: Number,
       required: [true, 'A tour must have a price']
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function(val) {
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below regular price'
+      }
+    },
     summary: {
       type: String,
       trim: true,
@@ -83,6 +99,12 @@ const tourSchema =  new mongoose.Schema({
   tourSchema.post(/^find/, function(docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
   });
+
+  // AGGREGATION MIDDLEWARE
+  tourSchema.pre('aggregate', function(next){
+    this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+    next();
+  })
 
   const Tour = mongoose.model('Tour', tourSchema);
 
