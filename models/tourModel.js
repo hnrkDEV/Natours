c662b7c52e6b88e1c0ff731ad39cf1ad9d81ee68
João Henrique <jhenrique.caval@hotmail.com>
2025-02-05 21:13:45 -0300

    Added query Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -52,7 +52,11 @@ const tourSchema =  new mongoose.Schema({
       type: Date,
       default: Date.now()
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false
+    }
   }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
@@ -67,11 +71,18 @@ const tourSchema =  new mongoose.Schema({
     this.slug = slugify(this.name, {lower: true});
     next();
   });
-  
-/*   tourSchema.post('save', function(doc, next) {
-    console.log(doc);
+
+  //  QUERY MIDDLEWARE
+  tourSchema.pre(/^find/, function(next) {
+    this.find({ secretTour: {$ne: true} });
+
+    this.start = Date.now();
     next();
-  }) */
+  });
+
+  tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  });
 
   const Tour = mongoose.model('Tour', tourSchema);
 
