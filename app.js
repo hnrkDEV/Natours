605aa320aa3380ffee02a4fb758a9c6e99491ed0
João Henrique <jhenrique.caval@hotmail.com>
2025-02-06 22:51:36 -0300

    fixing bug on routes

--- app.js
+++ app.js
@@ -18,17 +18,17 @@ app.use((req, res, next) => {
   next();
 });
 
+// 3- ROUTES
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
 app.all('*', (req,res,next) => {
-  
   res.status(404).json({
     status: 'fail',
     message: `Can't find ${req.originalUrl} on this server!`
   });
 });
 
-// 3- ROUTES
-
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
 
 module.exports = app;
