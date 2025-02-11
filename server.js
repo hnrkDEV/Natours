cd7074cffee209dc8f0181101abd6f6ad4f7ca92
João Henrique <jhenrique.caval@hotmail.com>
2025-02-11 02:16:18 -0300

    handling w errors

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT REJECTION! Shutting down...');
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -12,10 +18,19 @@ mongoose.connect(DB, {
   useFindAndModify: false,
   useUnifiedTopology: true
 }).then(() => {
-  console.log('DB Connection successful!')
+  console.log('DB Connection successful!');
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App runing on port ${port}...`);
 });
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
