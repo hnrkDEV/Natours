048c02d9739012d1a9c7aebfe58cd0f35795244f
João Henrique <jhenrique.caval@hotmail.com>
2025-02-12 02:04:05 -0300

    bcrypt in use

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
-
+const bcrypt = require('bcryptjs')
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -17,13 +17,37 @@ const userSchema = new mongoose.Schema({
       password: {
         type: String,
         required: [true, 'Password needed'],
-        minlength: 8
+        minlength: 8,
+        select: false
       },
       passwordConfirm: {
         type: String,
-        required: [true, 'Please confirm your password']
+        required: [true, 'Please confirm your password'],
+        validate: {
+          // THIS ONLY WORKS ON CREATE AND SAVE!!!
+          validator: function(el) {
+            return el === this.password;
+          },
+          message: "Passwords are not the same!"
+        }
       }
 });
+
+userSchema.pre('save', async function(next) {
+  // ONLY RUN THIS FUNCTION IF PASSWORD WAS ACTUALLY MODIFIED
+  if(!this.isModified('password')) return next();
+
+  // HASH THE PASSWORD WITH COST OF 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // DELETE THE PASSWORD FIELD TO NOT GO TO THE DB
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
