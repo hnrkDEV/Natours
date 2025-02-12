48a570cd90613807a312d8b788be68fe13a7dc3a
João Henrique <jhenrique.caval@hotmail.com>
2025-02-12 02:48:55 -0300

    adding protect route function

--- controllers/authController.js
+++ controllers/authController.js
@@ -49,3 +49,23 @@ exports.login = catchAsync(async (req,res, next) => {
         token
     });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+    // 1 - GETTING TOKEN AND CHECK OF IT'S THERE
+    let token;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        token = req.headers.authorization.split(' ')[1];
+    }
+
+    if(!token) {
+        return next(new AppError('You are not logged in! Please log in to get access.', 401));
+    };
+    // 2 - VERIFICATE TOKEN
+
+    // 3 - CHECK IF USER STILL EXISTS 
+
+    // 4 - CHECK IF USER CHANGED PASSWORD AFTER THE TOKEN WAS ISSUED
+
+
+    next();
+});
