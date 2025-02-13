d13b6711d16f158131df2bb38cdfe195ee867b62
João Henrique <jhenrique.caval@hotmail.com>
2025-02-13 03:01:31 -0300

    fixing bug of not returning jwt token

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,7 +5,7 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
 const signToken = id => {
-    jwt.sign({ id }, process.env.JWT_SECRET, {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRES_IN
     });
 }
