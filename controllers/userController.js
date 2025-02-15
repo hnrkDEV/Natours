76e5352e30959f4cd055857fe65b030bc3ef8978
João Henrique <jhenrique.caval@hotmail.com>
2025-02-15 02:26:31 -0300

    created delete user option and update data of user option

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,14 @@
-const fs = require('fs');
-const users = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/users.json`),
-);
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach(el => {
+    if(allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+}
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -14,6 +19,38 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     data: { users }
   });
 });
+
+exports.updateMe = catchAsync( async(req, res, next) => {
+  // 1 - CREATE ERROR IF USER POSTS PASSWORD DATA
+  if(req.body.password || req.body.passwordConfirm) {
+    return next(new AppError('This route is not for password updates. Please use /updateMyPassword.', 400));
+  };
+  // 2 - FILTERED OUT UNWANTED FILEDS NAMES THAT ARE NOT ALLOWED TO BE UPDATED
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3 - UPDATE THE USER DOCUMENT
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true, 
+    runValidators: true
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser
+    }
+  });
+});
+
+exports.deleteMe = catchAsync( async(req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false })
+
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
