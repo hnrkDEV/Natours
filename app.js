5887f7a8e077505582572809131cb85123bcbe85
João Henrique <jhenrique.caval@hotmail.com>
2025-02-16 23:06:43 -0300

    using rate-limit and helmet middleware

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 const express = require('express');
 const morgan = require('morgan');
-const AppError = require('./utils/appError')
-const globalErrorHandler = require('./controllers/errorController')
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const app = express();
 
-// 1 - MIDDLEWARES
+// 1 - GLOBAL MIDDLEWARES
+// SET SECURITY HTTP HEADERS
+app.use(helmet());
+
+// DEVELOPMENT LOGGING
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// LIMIT REQUESTS FROM SAME API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!' 
+});
+app.use('/api', limiter);
+
+// BODY PARSER, READING DATA FROM BODY INTO REQ.BODY
+app.use(express.json({ limit: '10kb' }));
+
+// SERVING STATIC FILES
 app.use(express.static(`${__dirname}/public`));
 
+// TEST MIDDLEWARE
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 // 3- ROUTES
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
