c6ea55d84429430a5db8d7363f5ea5f5d7185b33
João Henrique <jhenrique.caval@hotmail.com>
2025-02-17 21:41:30 -0300

    using mongo-sanitize, xss and hpp to protect nosql inject etc

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const app = express();
 
@@ -28,6 +31,17 @@ app.use('/api', limiter);
 
 // BODY PARSER, READING DATA FROM BODY INTO REQ.BODY
 app.use(express.json({ limit: '10kb' }));
+
+// DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
+app.use(mongoSanitize());
+
+// DATA SANITIZATION AGAINST XSS
+app.use(xss());
+
+// PREVENT PARAMETER POLLUTION
+app.use(hpp({
+  whitelist: ['duration', 'ratingsQuantity', 'ratingsAverage', 'maxGroupSize', 'difficulty', 'price']
+}));
 
 // SERVING STATIC FILES
 app.use(express.static(`${__dirname}/public`));
