c051aaf20830212fd7f2dfbdc06778b6093d9490
João Henrique <114232200+hnrkDEV@users.noreply.github.com>
2025-03-12 01:42:11 -0300

    creating child referencing on pre-save tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./userModel');
 
 const tourSchema =  new mongoose.Schema({
     name: {
@@ -97,7 +98,8 @@ const tourSchema =  new mongoose.Schema({
         description: String,
         day: Number
       }
-    ]
+    ],
+    guides: Array
   }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
@@ -111,6 +113,12 @@ const tourSchema =  new mongoose.Schema({
   tourSchema.pre('save', function(next) {
     this.slug = slugify(this.name, {lower: true});
     next();
+  });
+
+  tourSchema.pre('save', async function(next) {
+    const guidesPromises = this.guides.map(async id => User.findById(id));
+    this.guides = await Promise.all(guidesPromises)
+    next()
   });
 
   //  QUERY MIDDLEWARE
