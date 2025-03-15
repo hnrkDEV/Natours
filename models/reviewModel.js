0deb71bdc971193687f1a222854a0b0ff259f840
João Henrique <jhenrique.caval@hotmail.com>
2025-03-15 16:38:32 -0300

    optimizing the populate func

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -35,9 +35,7 @@ const reviewSchema = new mongoose.Schema({
     this.populate({
         path: 'tour',
         select: 'name'
-    });
-
-    this.populate({
+    }).populate({
         path: 'user',
         select: 'name photo'
     });
