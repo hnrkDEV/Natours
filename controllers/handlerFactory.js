a02ea5499efaeb47584a952da03be695831283a9
João Henrique <jhenrique.caval@hotmail.com>
2025-03-18 00:35:54 -0300

    creating update handler

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -5,11 +5,26 @@ exports.deleteOne = Model => catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndDelete(req.params.id);
   
     if (!doc) {
-      return next(new AppError(`No documents found with that ID`, 404));
+      return next(new AppError(`No document found with that ID`, 404));
     };
   
     res.status(204).json({
       status: 'success',
-      message: 'Tour deleted successfully!'
+      data: null
     });
   });
+
+exports.updateOne = Model => catchAsync(async (req, res, next) => {
+  const doc = await Model.findByIdAndUpdate(req.params.id)
+
+  if (!doc) {
+    return next(new AppError(`No document found with that ID`, 404));
+  };
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: doc
+    }
+  });
+})
