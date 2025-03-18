bb4779d3edf2cdaed3eb9d656b8f749270c1577e
João Henrique <jhenrique.caval@hotmail.com>
2025-03-17 23:07:19 -0300

    adding delete on router

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,12 +4,16 @@ const authController = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
-router.route('/')
+router
+.route('/')
 .get(reviewController.getAllReviews)
 .post(
     authController.protect,
     authController.restrictTo('user'),
     reviewController.createReview
 );
+
+router
+.route('/:id').delete(reviewController.deleteReview);
 
 module.exports = router;
