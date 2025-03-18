de8421170cac2cf971493f2163cf8d81e2e8377c
João Henrique <jhenrique.caval@hotmail.com>
2025-03-17 23:06:48 -0300

    Adding delete handler

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,6 +2,7 @@ const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -68,7 +69,7 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     });
 });
 
-exports.deleteTour = catchAsync(async (req, res, next) => {
+/* exports.deleteTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findByIdAndDelete(req.params.id);
 
   if (!tour) {
@@ -79,7 +80,9 @@ exports.deleteTour = catchAsync(async (req, res, next) => {
     status: 'success',
     message: 'Tour deleted successfully!'
   });
-});
+}); */
+
+exports.deleteTour = factory.deleteOne(Tour);
 
 exports.getTourStats = catchAsync(async (req, res, next) => {
     const stats = await Tour.aggregate([
