9b4e475bf99f3c26219d1b1aef60fd5f6cf914a9
João Henrique <jhenrique.caval@hotmail.com>
2025-03-20 20:16:28 -0300

    creating getOne and getAll handler functions

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,5 +1,6 @@
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.deleteOne = Model => catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndDelete(req.params.id);
@@ -39,4 +40,35 @@ exports.createOne = Model => catchAsync(async (req, res, next) => {
       data: doc
     }
   });
+});
+
+exports.getOne = (Model, popOptions) => catchAsync(async (req, res, next) => {
+  let query = Model.findById(req.params.id);
+  if(popOptions) query = query.populate(popOptions);
+  const doc = await query;
+
+  if (doc.length === 0) {
+    return next(new AppError('No document found with that ID', 404));
+  };
+
+  res.status(200).json({
+    status: 'success',
+    data: { doc }
+  });
+});
+
+exports.getAll = Model => catchAsync(async (req, res, next) => {
+    const features = new APIFeatures(Model.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+      
+    const doc = await features.query;
+
+    res.status(200).json({
+      status: 'success',
+      results: doc.length,
+      data: { doc }
+    });
 });
