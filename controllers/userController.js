ef4be7f107a6e7086d169952de6f8d3247841be7
João Henrique <jhenrique.caval@hotmail.com>
2025-03-19 23:16:00 -0300

    Deleting create user (already have signup on authController)

--- controllers/userController.js
+++ controllers/userController.js
@@ -58,11 +58,7 @@ exports.getUser = (req, res) => {
     message: 'This route is not yet defined!',
   });
 };
-exports.createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
+
 exports.updateUser = factory.updateOne(User);
+
 exports.deleteUser = factory.deleteOne(User);
