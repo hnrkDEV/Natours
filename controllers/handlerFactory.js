b2591ae9be1f1eabbd46ccd74070b4f6fc13c15e
João Henrique <jhenrique.caval@hotmail.com>
2025-03-21 00:42:12 -0300

    adding the filter object on get all handler

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -58,7 +58,12 @@ exports.getOne = (Model, popOptions) => catchAsync(async (req, res, next) => {
 });
 
 exports.getAll = Model => catchAsync(async (req, res, next) => {
-    const features = new APIFeatures(Model.find(), req.query)
+
+    // To allow for nested GET reviews on tour
+    let filter = {}
+    if(req.params.tourId) filter = {tour: req.params.tourId}
+
+    const features = new APIFeatures(Model.find(filter), req.query)
       .filter()
       .sort()
       .limitFields()
