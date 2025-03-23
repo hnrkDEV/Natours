483d1a6893b7bb8b6ae72682ddce75177e33704b
João Henrique <jhenrique.caval@hotmail.com>
2025-03-23 03:12:27 -0300

    Revert "creating an endpoint to locate the tours based on the radius with our latitude and long"
    This reverts commit 576e6afa235cbe22037b260ccf35bb30fa7cea05.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -83,24 +83,3 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
       data: { plan }
     });
 });
-
-exports.getTourWithin = catchAsync(async (req, res, next) => {
-  const { distance, latlng, unit } = req.params;
-  const [lat, lng] = latlng.split(',');
-
-  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
-
-  if(!lat || !lng) {
-    next(new AppError('Please provide latitude and longitute in the format lat, lng', 400));
-  };
-
-  const tours = await Tour.find({ startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius]}}});
-
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      data: tours
-    }
-  });
-});
