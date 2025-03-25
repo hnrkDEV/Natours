b072cf1247106787f2da3fcc40770a0e223b55be
João Henrique <jhenrique.caval@hotmail.com>
2025-03-25 01:33:52 -0300

    setting up pug configurations and viewRouter

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const AppError = require('./utils/appError');
@@ -5,6 +6,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
@@ -13,7 +15,12 @@ const hpp = require('hpp');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // 1 - GLOBAL MIDDLEWARES
+// SERVING STATIC FILES
+app.use(express.static(path.join(__dirname, 'public')));
 // SET SECURITY HTTP HEADERS
 app.use(helmet());
 
@@ -44,9 +51,6 @@ app.use(hpp({
   whitelist: ['duration', 'ratingsQuantity', 'ratingsAverage', 'maxGroupSize', 'difficulty', 'price']
 }));
 
-// SERVING STATIC FILES
-app.use(express.static(`${__dirname}/public`));
-
 // TEST MIDDLEWARE
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -54,6 +58,7 @@ app.use((req, res, next) => {
 });
 
 // 3- ROUTES
+app.use('', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
