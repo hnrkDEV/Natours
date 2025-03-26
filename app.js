1b9cfa02bc3469b9375d52d5f79facd451ffc158
João Henrique <jhenrique.caval@hotmail.com>
2025-03-26 03:32:25 -0300

    implementing cookie-parser

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser')
 
 const app = express();
 
@@ -39,6 +40,7 @@ app.use('/api', limiter);
 
 // BODY PARSER, READING DATA FROM BODY INTO REQ.BODY
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 // DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
 app.use(mongoSanitize());
@@ -54,13 +56,14 @@ app.use(hpp({
 // TEST MIDDLEWARE
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(req.cookies);
   next();
 });
 
 app.use((req, res, next) => {
   res.setHeader(
     'Content-Security-Policy',
-    "script-src 'self' https://unpkg.com blob:; worker-src 'self' blob:;",
+    "script-src 'self' https://unpkg.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net blob:; worker-src 'self' blob:;"
   );
   next();
 });
