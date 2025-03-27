b001bf0cceeb8315002ff135be003fedc6cce959
João Henrique <jhenrique.caval@hotmail.com>
2025-03-27 02:20:18 -0300

    refatoring the code

--- public/js/index.js
+++ public/js/index.js
@@ -2,22 +2,28 @@ import '@babel/polyfill';
 import { displayMap  } from './maplibre';
 import { login } from './login';
 
-const form = document.querySelector('.form');
-if (!form) {
-  console.error('Formulário não encontrado!');
-  return;
+// DOM ELEMENTS
+
+const mapBox = document.getElementById('map');
+
+// DELEGATION
+
+if(mapBox) {
+    const locations = JSON.parse(document.getElementById('map').dataset.locations);
+    displayMap(locations);
 }
 
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
+const form = document.querySelector('.form');
 
-displayMap(locations);
+if (form) {
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        console.log('formulário submetido');
+      
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+      
+        login(email, password);
+      });
+}
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  console.log('formulário submetido');
-
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-
-  login(email, password);
-});
