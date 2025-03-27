97d5e9ab62dee7b783e0d054bb35dca135c61e2f
João Henrique <jhenrique.caval@hotmail.com>
2025-03-27 02:07:14 -0300

    implementing axios by npm and configurating to export

--- public/js/login.js
+++ public/js/login.js
@@ -1,38 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const login = async (email, password) => {
-      try {
-        const res = await axios({
-          method: 'POST',
-          url: 'http://127.0.0.1:8000/api/v1/users/login',
-          data: { email, password }
-        });
+import axios from 'axios';
 
-        if(res.data.status === 'success') {
-            alert('Logged in successfully!');
-            window.setTimeout(() => {
-                location.assign('/')
-            });
-        }
-        console.log(res);
-      } catch (err) {
-        alert(err.response.data.message)
-      }
-    };
-  
-    const form = document.querySelector('.form');
-    if (!form) {
-      console.error('Formulário não encontrado!');
-      return;
+export const login = async (email, password) => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://127.0.0.1:8000/api/v1/users/login',
+      data: { email, password }
+    })
+    if(res.data.status === 'success') {
+        alert('Logged in successfully!');
+        window.setTimeout(() => {
+            location.assign('/')
+        });
     }
-  
-    form.addEventListener('submit', e => {
-      e.preventDefault();
-      console.log('formulário submetido');
-  
-      const email = document.getElementById('email').value;
-      const password = document.getElementById('password').value;
-  
-      login(email, password);
-    });
-  });
-  
+    console.log(res);
+  } catch (err) {
+    alert(err.response.data.message)
+  }
+}
