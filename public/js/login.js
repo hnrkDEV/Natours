da0bb6f8a334f8c108bf781f9c6715d118a64fe4
João Henrique <jhenrique.caval@hotmail.com>
2025-03-27 02:46:36 -0300

    implementing the alert

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { showAlert } from './alerts';
 
 export const login = async (email, password) => {
   try {
@@ -8,13 +9,12 @@ export const login = async (email, password) => {
       data: { email, password }
     })
     if(res.data.status === 'success') {
-        alert('Logged in successfully!');
+        showAlert('success', 'Logged in successfully!')
         window.setTimeout(() => {
             location.assign('/')
-        });
+        }, 1500);
     }
-    console.log(res);
   } catch (err) {
-    alert(err.response.data.message)
-  }
-}
+    showAlert('error', err.response.data.message)
+  };
+};
