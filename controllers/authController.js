9d3d092fadbf37030e0ac5e47fce6a5611a16789
João Henrique <jhenrique.caval@hotmail.com>
2025-03-28 04:25:36 -0300

    Creating the Logout function and removing the CatchAsync from "isLoggedIn" Middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -62,6 +62,15 @@ exports.login = catchAsync(async (req,res, next) => {
     createSendToken(user, 200, res);
 });
 
+exports.logout = (req, res) => {
+    res.cookie('jwt', 'loggedout', {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true
+    });
+    
+    res.status(200).json({ status: 'success' });
+};
+
 exports.protect = catchAsync(async (req, res, next) => {
     // 1 - GETTING TOKEN AND CHECK OF IT'S THERE
     let token;
@@ -93,9 +102,9 @@ exports.protect = catchAsync(async (req, res, next) => {
 });
 
 // ONLY FOR RENDERED PAGES, NO ERRORS!
-exports.isLoggedIn = catchAsync(async (req, res, next) => {
+exports.isLoggedIn = async (req, res, next) => {
     if(req.cookies.jwt) {
-    
+    try {
     // 1 - VERIFY TOKEN
     const decoded = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRET);
     
@@ -113,8 +122,12 @@ exports.isLoggedIn = catchAsync(async (req, res, next) => {
     res.locals.user = currentUser;
     return next();
     }
+     catch (error) {
+        return next();
+        }
+    }
     next();
-});
+};
 
 exports.restrictTo = (...roles) => {
     return (req, res, next) => {
