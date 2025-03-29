2863bc1f8a7223af54e7e336e02e53f605c54c4f
João Henrique <jhenrique.caval@hotmail.com>
2025-03-29 02:14:17 -0300

    setting the account template render

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -34,8 +34,13 @@ exports.getTour = catchAsync(async (req, res, next) => {
   });
 
 exports.getLoginForm = (req, res) => {
-
   res.status(200).render('login', {
     title: 'Log into your account'
+  });
+};
+
+exports.getAccount = (req, res) => {
+  res.status(200).render('account', {
+    title: 'Your account'
   });
 };
