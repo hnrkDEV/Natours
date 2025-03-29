827286b58a628909cbe9337afbfa51a38b6c03da
João Henrique <jhenrique.caval@hotmail.com>
2025-03-29 03:05:55 -0300

    implementing express.urlencoded

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.use('/api', limiter);
 
 // BODY PARSER, READING DATA FROM BODY INTO REQ.BODY
 app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
 
 // DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
